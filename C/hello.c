6a95c9536ee129c604f91f45fc50e34cb8753029
A1X71 <william.wang.chn@icloud.com>
2017-06-03 23:01:48 +0800

    c program practice

---
+++ C/hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+	float a, b, c; 
+	printf("input a,b,c:");
+	scanf("%f,%f,%f", &a,&b,&c);
+	printf("Hello World Output\n a= %f c=%f", a,b,c);
+	printf("\n");
+	return 0;
+}
