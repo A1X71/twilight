839dfe5c1f92f87cca6a7df3be558ee3b98ea65e
A1X71 <william.wang.chn@icloud.com>
2017-06-20 17:06:02 +0800

    Enum value and Description converter

---
+++ SCA.WPF/SCA.BusinessLib/Utility/EnumUtility.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using System.ComponentModel;
+/* ==============================
+*
+* Author     : William
+* Create Date: 2017/6/20 16:54:59
+* FileName   : EnumUtility
+* Description: 枚举操作
+* Version：V1
+* ===============================
+*/
+namespace SCA.BusinessLib.Utility
+{
+    public class EnumUtility
+    {
+        /// <summary>
+        ///  根据属性描述值,取得枚举值
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="description"></param>
+        /// <returns></returns>
+        public static string GetEnumName(System.Type value,string description)
+        {
+            FieldInfo[] fis = value.GetFields();
+            foreach (FieldInfo fi in fis)
+            {
+                DescriptionAttribute[] attributes =
+                (DescriptionAttribute[])fi.GetCustomAttributes
+                (typeof(DescriptionAttribute), false);
+                if (attributes.Length > 0)
+                {
+                    if (attributes[0].Description == description)
+                    {
+                        return fi.Name;
+                    }
+                }
+            }
+            return description;
+        }
+    }
+}
